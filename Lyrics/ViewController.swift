7e2129e1498030f6943c1190aa6685ac19c31e63
TheShubhamArya <shubhamarya11099@gmail.com>
2021-11-10 11:35:09 -0600

    Initial Commit

---
+++ Lyrics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lyrics
+//
+//  Created by Shubham Arya on 11/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
